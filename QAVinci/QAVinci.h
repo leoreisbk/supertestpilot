1689b895ca53ee470dbbb25bd0807510481ea09f
Flávio Caetano <caetano@work.co>
2023-03-13 14:32:10 -0400

    Initial Commit

---
+++ QAVinci/QAVinci.h
@@ -0,0 +1,18 @@
+//
+//  QAVinci.h
+//  QAVinci
+//
+//  Created by Flávio Caetano on 3/13/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QAVinci.
+FOUNDATION_EXPORT double QAVinciVersionNumber;
+
+//! Project version string for QAVinci.
+FOUNDATION_EXPORT const unsigned char QAVinciVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QAVinci/PublicHeader.h>
+
+
