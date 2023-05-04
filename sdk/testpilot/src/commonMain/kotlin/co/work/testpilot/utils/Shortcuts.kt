f28838f2d618d201ae1c4375da63978a0dfe8c22
Igor Lira <lira@work.co>
2023-05-04 11:55:16 -0700

    fix(KMM): fix crash when element not found on iOS

--- sdk/testpilot/src/commonMain/kotlin/co/work/testpilot/utils/Shortcuts.kt
+++ sdk/testpilot/src/commonMain/kotlin/co/work/testpilot/utils/Shortcuts.kt
@@ -1,5 +1,13 @@
 package co.work.testpilot.utils
 
+fun <T> tryOrNull(callback: () -> T): T? {
+    return try {
+        callback()
+    } catch (err: Throwable) {
+        null
+    }
+}
+
 suspend fun <T> suspendTryOrNull(callback: suspend () -> T): T? {
     return try {
         callback()
--- sdk/testpilot/src/iosMain/kotlin/co/work/testpilot/extensions/XCUIElement.kt
+++ sdk/testpilot/src/iosMain/kotlin/co/work/testpilot/extensions/XCUIElement.kt
@@ -1,13 +1,16 @@
 package co.work.testpilot.extensions
 
+import co.work.testpilot.utils.tryOrNull
 import platform.XCTest.XCUIElement
 import platform.XCTest.XCUIElementSnapshotProtocol
 import platform.XCTest.XCUIElementType
 
-fun XCUIElementSnapshotProtocol.findIn(parent: XCUIElement) = parent.firstMatch(elementType, label)
+fun XCUIElementSnapshotProtocol.findIn(parent: XCUIElement) = parent.firstMatchOrNull(elementType, label)
 fun XCUIElement.waitForExistenceIfNecessary(timeoutSeconds: Double): Boolean {
     return if (exists) true else {
         return waitForExistenceWithTimeout(timeoutSeconds)
     }
 }
-fun XCUIElement.firstMatch(type: XCUIElementType, label: String) = descendantsMatchingType(type).matchingIdentifier(label).firstMatch
+fun XCUIElement.firstMatchOrNull(type: XCUIElementType, label: String): XCUIElement? {
+    return tryOrNull { descendantsMatchingType(type).matchingIdentifier(label).firstMatch }
+}
